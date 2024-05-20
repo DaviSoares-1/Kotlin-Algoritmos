f2081522015f03d772372d51a22d23028703d461
Davi Soares <davi_soaresp@outlook.com>
2024-05-19 22:31:18 -0300

    Desafio #011 Concluído.

--- src/main/kotlin/Sequencias_Basicas/algoritmo-011.kt
+++ src/main/kotlin/Sequencias_Basicas/algoritmo-011.kt
@@ -1,5 +1,25 @@
 package Sequencias_Basicas
+import java.util.*
 
 fun main() {
+  /* Desafio 011: Desenvolva uma lógica que leia os valores de A, B e C de uma equação do segundo grau e mostre o valor de Delta.*/
 
+  println("Informe os coeficientes de uma equação do segundo grau.")
+
+  print("Digite o valor do coeficiente A (diferente de 0): ")
+  val valor1 = Scanner(System.`in`)
+  val A: Int = valor1.nextInt()
+
+  print("Digite o valor do coeficiente B: ")
+  val valor2 = Scanner(System.`in`)
+  val B: Int = valor2.nextInt()
+
+  print("Digite o valor do coeficiente C: ")
+  val valor3 = Scanner(System.`in`)
+  val C: Int = valor3.nextInt()
+
+  // Math.pow(base, expoente);
+  val delta = Math.pow(B.toDouble(), 2.0).toInt() - 4 * A * C
+
+  println("O delta da equação (${A})x²+(${B})x+(${C}) é: $delta")
 }
