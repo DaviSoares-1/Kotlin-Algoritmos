8b54766ff371f94d7348f7de203f46aba615147c
Davi Soares <davi_soaresp@outlook.com>
2024-07-03 19:48:43 -0300

    algoritmo #009 corrigido - import não necessário removido

--- src/main/kotlin/Sequencias_Basicas/algoritmo-009.kt
+++ src/main/kotlin/Sequencias_Basicas/algoritmo-009.kt
@@ -1,7 +1,6 @@
 package Sequencias_Basicas
 
 import java.util.Scanner
-import kotlin.math.roundToInt
 
 fun main() {
   /* Desafio 009: Faça um algoritmo que leia quanto dinheiro uma pessoa tem na carteira (em R$) e mostre quantos dólares ela pode comprar. Considere US$1,00 = R$3,45.*/
